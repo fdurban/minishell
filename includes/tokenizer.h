86a04dc18468391806366db51c4c937805106507
Fernando Durban Brizio <fdurban-@c2r5s3.42madrid.com>
2025-05-07 16:51:19 +0200

    try to dicide all the functions and home and see how to add spaces and redirections

--- includes/tokenizer.h
+++ includes/tokenizer.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokenizer.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fernando <fernando@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fdurban- <fdurban-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 13:22:28 by fdurban-          #+#    #+#             */
-/*   Updated: 2025/05/06 16:40:21 by fernando         ###   ########.fr       */
+/*   Updated: 2025/05/07 16:04:50 by fdurban-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,24 @@
 // "state"
 typedef enum e_word_type
 {
-	WORD_START,
-	WORD_STANDARD,
-	WORD_SINGLE_QUOTE,
-	WORD_DOUBLE_QUOTE,
-	WORD_REDIRECT_IN,
-	WORD_REDIRECT_OUT,
-	WORD_REDIRECT_APPEND,
-	WORD_HEREDOC,
-	WORD_SPACE,
-	WORD_SPACE_AFTER_REDIRECT,
-	WORD_END_OF_SINGLE_QUOTE,
-	WORD_END_OF_DOUBLE_QUOTE,
-	WORD_END_STANDARD,
-	WORD_END_STANDARD_TO_DOUBLE_QUOTE,
-	WORD_END_STANDARD_TO_SINGLE_QUOTE,
-	WORD_END,
-	WORD_ERROR,
-	NUM_WORDS
+	W_START,
+	W_STNDR,
+	W_SINGQ,
+	W_DOUBQ,
+	W_REDIN,
+	W_REDOU,
+	W_REDAP,
+	W_HRDOC,
+	W_SPACE,
+	W_SARED,
+	W_EOFSQ,
+	W_EOFDQ,
+	W_EOFST,
+	W_EOSTD,
+	W_EOSTS,
+	W___END,
+	W_ERROR,
+	W_TOTAL
 } t_word_type;
 
 typedef enum e_input_tokenizer
@@ -55,6 +55,46 @@ typedef struct s_command_part
 	struct s_command_part	*next;
 } t_command_part;
 
-char**	tokenize(char *valid_input);
+	// const int	matrix[W_TOTAL][NUM_INPUT] = {
+	// {W_START, W_STNDR, W___END, W_SINGQ, W_DOUBQ, W_REDIN, W_REDOU}, //W_START
+	// {W_EOFST, W_STNDR, W___END, W_EOSTS, W_EOSTD, W_EOFST, W_EOFST}, // W_STNDR
+	// {W_SINGQ, W_SINGQ, W_ERROR, W_EOFSQ, W_SINGQ, W_SINGQ, W_SINGQ}, // WORD_SINGLE QUOTE
+	// {W_DOUBQ, W_DOUBQ, W_STNDR, W_DOUBQ, W_EOFDQ, W_DOUBQ, W_DOUBQ}, // WORD_DOUBLE QUOTE
+	// {W_SPACE, W_STNDR, W_ERROR, W_SINGQ, W_DOUBQ, W_HRDOC, W_ERROR}, // REDIRECT_IN
+	// {W_REDOU, W_REDOU, W___END, W_REDOU, W_REDOU, W_REDIN, W_REDAP}, // REDIRECT_OUT
+	// {W_SARED, W_STNDR, W_ERROR, W_SINGQ, W_DOUBQ, W_ERROR, W_ERROR}, // REDIRECT_APPEND
+	// {W_SARED, W_STNDR, W_ERROR, W_SINGQ, W_DOUBQ, W_ERROR, W_ERROR}, // HERE_DOC
+	// {W_SPACE, W_STNDR, W___END, W_SINGQ, W_DOUBQ, W_REDIN, W_REDOU}, // SPACE AFTER WORD
+	// {W_SARED, W_STNDR, W_ERROR, W_SINGQ, W_DOUBQ, W_ERROR, W_ERROR}, // SPACE AFTER REDIRECT
+	// {W_SPACE, W_STNDR, W___END, W_SINGQ, W_DOUBQ, W_REDIN, W_REDOU}, // END OF SINGLE QUOTE
+	// {W_SPACE, W_STNDR, W___END, W_SINGQ, W_DOUBQ, W_REDIN, W_REDOU}, // END OF DOUBLE QUOTE
+	// {W_SPACE, W_STNDR, W___END, W_SINGQ, W_DOUBQ, W_REDIN, W_REDOU}, // END OF STANDARD
+	// {W_DOUBQ, W_DOUBQ, W___END, W_EOFDQ, W_EOFDQ, W_DOUBQ, W_DOUBQ}, // END OF STANDARD TO DOUBLE QUOTE
+	// {W_SINGQ, W_SINGQ, W___END, W_EOFSQ, W_SINGQ, W_SINGQ, W_SINGQ} // END OF STANDARD TO DOUBLE QUOTE
+	// };
 
+char**	tokenize(char *valid_input);
+void 	get_word_transition_matrix(int matrix[17][7]);
 #endif
+
+
+/*
+	WORD_START ->						W_START
+	WORD_STANDARD->						W_STNDR
+	WORD_SINGLE_QUOTE ->				W_SINGQ
+	WORD_DOUBLE_QUOTE ->				W_DOUBQ
+	WORD_REDIRECT_IN ->					W_REDIN
+	WORD_REDIRECT_OUT ->				W_REDOU
+	WORD_REDIRECT_APPEND ->				W_REDAP
+	WORD_HEREDOC ->						W_HRDOC
+	WORD_SPACE ->						W_SPACE
+	WORD_SPACE_AFTER_REDIRECT			W_SARED
+	WORD_END_OF_SINGLE_QUOTE			W_EOFSQ
+	WORD_END_OF_DOUBLE_QUOTE			W_EOFDQ
+	WORD_END_STANDARD					W_EOFST
+	WORD_END_STANDARD_TO_DOUBLE_QUOTE	W_EOSTD
+	WORD_END_STANDARD_TO_SINGLE_QUOTE,	W_EOSTS
+	WORD_END							W___END
+	WORD_ERROR							W_ERROR
+	NUM_WORDS							W_TOTAL
+*/
--- tokenize/tokenize.c
+++ tokenize/tokenize.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tokenize.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fernando <fernando@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fdurban- <fdurban-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 12:55:33 by fdurban-          #+#    #+#             */
-/*   Updated: 2025/05/06 17:12:07 by fernando         ###   ########.fr       */
+/*   Updated: 2025/05/07 16:39:01 by fdurban-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ int	get_token_type(char c)
 		return (0);
 }
 //char command
-char	*extract_command(char *str,int *i, const int matrix[NUM_WORDS][NUM_INPUT], t_word_type word_type,  t_word_type previous_word_type)
+char	*extract_command(char *str,int *i, const int matrix[W_TOTAL][NUM_INPUT], t_word_type word_type,  t_word_type previous_word_type)
 {
 	int					start;
 	t_input_tokenizer	input;
@@ -73,29 +73,26 @@ char	*extract_command(char *str,int *i, const int matrix[NUM_WORDS][NUM_INPUT], 
 	t_word_type			end_type;
 
 	start = *i;
-	if(word_type == WORD_DOUBLE_QUOTE && (previous_word_type == WORD_END_STANDARD_TO_DOUBLE_QUOTE || previous_word_type == WORD_END_STANDARD_TO_SINGLE_QUOTE))
+	if((word_type == W_DOUBQ || word_type == W_SINGQ) && (previous_word_type == W_EOSTD || previous_word_type == W_EOSTS))
 		start = *i - 1;
-	if (word_type == WORD_DOUBLE_QUOTE)
-		end_type = WORD_END_OF_DOUBLE_QUOTE;
-	else if (word_type == WORD_SINGLE_QUOTE)
-		end_type = WORD_END_OF_SINGLE_QUOTE;
+	if (word_type == W_DOUBQ)
+		end_type = W_EOFDQ;
+	else if (word_type == W_SINGQ)
+		end_type = W_EOFSQ;
 	else
-		end_type = WORD_END_STANDARD;
-	while (word_type == WORD_DOUBLE_QUOTE || word_type == WORD_SINGLE_QUOTE || word_type == WORD_STANDARD)
+		end_type = W_EOFST;
+	while (word_type == W_DOUBQ || word_type == W_SINGQ || word_type == W_STNDR)
 	{
 		(*i)++;
 		input = get_token_type(str[*i]);
 		word_type = matrix[word_type][input];
 	}
 	result = ft_substr(str, start, *i - start);
-	if (end_type == WORD_END_OF_SINGLE_QUOTE || end_type == WORD_END_OF_DOUBLE_QUOTE)
+	if (end_type == W_EOFDQ || end_type == W_EOFSQ)
 		result = ft_substr(str, start + 1, *i - start - 1);
-	printf("valor de i es %d\n", *i);
-	printf("valor de start es %d\n", start);
-	printf("Numero de caracteres impresos son %d\n", *i - start);
 	return (result);
 }
-char	**tokenize(char *valid_command)
+char	**tokenize_help(const int matrix[W_TOTAL][NUM_INPUT], char *valid_command)
 {
 	char				**tokens;
 	t_input_tokenizer	input;
@@ -103,87 +100,112 @@ char	**tokenize(char *valid_command)
 	t_word_type			word_type;
 	char				*random_command;
 	t_word_type			previous_word_type;
+	t_command_part		*command;
+	t_command_part		*lst = NULL;
+
 
 	i = 0;	
 	tokens = ft_split(valid_command, '|');
 	random_command = NULL;
+	command = NULL;
 	// space //letter // end // single quote //double quote //redirect IN // redirect out
-	const int	matrix[NUM_WORDS][NUM_INPUT] = {
-	{WORD_START, WORD_STANDARD, WORD_END, WORD_SINGLE_QUOTE, WORD_DOUBLE_QUOTE, WORD_REDIRECT_IN, WORD_REDIRECT_OUT}, //WORD_START
-	{WORD_END_STANDARD, WORD_STANDARD, WORD_END, WORD_END_STANDARD_TO_SINGLE_QUOTE, WORD_END_STANDARD_TO_DOUBLE_QUOTE, WORD_END_STANDARD, WORD_END_STANDARD}, // WORD_STANDARD
-	{WORD_SINGLE_QUOTE, WORD_SINGLE_QUOTE, WORD_ERROR, WORD_END_OF_SINGLE_QUOTE,WORD_SINGLE_QUOTE, WORD_SINGLE_QUOTE}, // WORD_SINGLE QUOTE
-	{WORD_DOUBLE_QUOTE, WORD_DOUBLE_QUOTE, WORD_STANDARD, WORD_DOUBLE_QUOTE, WORD_END_OF_DOUBLE_QUOTE, WORD_DOUBLE_QUOTE, WORD_DOUBLE_QUOTE}, // WORD_DOUBLE QUOTE
-	{WORD_SPACE, WORD_STANDARD, WORD_ERROR, WORD_SINGLE_QUOTE, WORD_DOUBLE_QUOTE, WORD_HEREDOC}, // REDIRECT_IN
-	{WORD_REDIRECT_OUT, WORD_REDIRECT_OUT, WORD_END, WORD_REDIRECT_OUT, WORD_REDIRECT_OUT,WORD_REDIRECT_IN, WORD_REDIRECT_APPEND}, // REDIRECT_OUT
-	{WORD_SPACE_AFTER_REDIRECT, WORD_STANDARD, WORD_ERROR, WORD_SINGLE_QUOTE, WORD_DOUBLE_QUOTE, WORD_ERROR}, // REDIRECT_APPEND
-	{WORD_SPACE_AFTER_REDIRECT, WORD_STANDARD, WORD_ERROR, WORD_SINGLE_QUOTE, WORD_DOUBLE_QUOTE, WORD_ERROR}, // HERE_DOC
-	{WORD_SPACE, WORD_STANDARD, WORD_END, WORD_SINGLE_QUOTE, WORD_DOUBLE_QUOTE, WORD_REDIRECT_IN, WORD_REDIRECT_OUT}, // SPACE AFTER WORD
-	{WORD_SPACE_AFTER_REDIRECT, WORD_STANDARD, WORD_ERROR, WORD_SINGLE_QUOTE, WORD_DOUBLE_QUOTE, WORD_ERROR, WORD_ERROR}, // SPACE AFTER REDIRECT
-	{WORD_SPACE, WORD_STANDARD, WORD_END, WORD_SINGLE_QUOTE, WORD_DOUBLE_QUOTE, WORD_REDIRECT_IN, WORD_REDIRECT_OUT}, // END OF SINGLE QUOTE
-	{WORD_SPACE, WORD_STANDARD, WORD_END, WORD_SINGLE_QUOTE, WORD_DOUBLE_QUOTE, WORD_REDIRECT_IN, WORD_REDIRECT_OUT}, // END OF DOUBLE QUOTE
-	{WORD_SPACE, WORD_STANDARD, WORD_END, WORD_SINGLE_QUOTE, WORD_DOUBLE_QUOTE, WORD_REDIRECT_IN, WORD_REDIRECT_OUT},// END OF STANDARD
-	{WORD_DOUBLE_QUOTE, WORD_DOUBLE_QUOTE, WORD_END, WORD_END_OF_DOUBLE_QUOTE, WORD_END_OF_DOUBLE_QUOTE, WORD_DOUBLE_QUOTE, WORD_DOUBLE_QUOTE},// END OF STANDARD TO DOUBLE QUOTE
-	{WORD_SINGLE_QUOTE, WORD_SINGLE_QUOTE, WORD_END, WORD_END_OF_SINGLE_QUOTE, WORD_SINGLE_QUOTE, WORD_SINGLE_QUOTE, WORD_SINGLE_QUOTE}// END OF STANDARD TO DOUBLE QUOTE
-	};
-	word_type = WORD_START;
-	while (valid_command[i] != '\0' && word_type != WORD_END)
+	word_type = W_START;
+	while (valid_command[i] != '\0' && word_type != W___END)
 	{
 		previous_word_type = word_type;
 		input = get_token_type(valid_command[i]);
 		word_type = matrix[word_type][input];
-		//--------------------------------------------------------------------
-		printf("---------------------------------------\n");
-		printf("Letra %c\n", valid_command[i]);
-		if(word_type == WORD_START)
-			printf("word_type START and i value is : %d\n", i);
-		if(word_type == WORD_STANDARD)
-			printf("word_type WORD and i value is : %d\n", i);
-		if(word_type == WORD_SINGLE_QUOTE)
-			printf("word_type SINGLE QUOTE and i value is : %d\n", i);
-		if(word_type == WORD_DOUBLE_QUOTE)
-			printf("word_type DOUBLE and i value is : %d\n", i);
-		if(word_type == WORD_REDIRECT_IN)
-			printf("word_type REDIR in and i value is : %d\n", i);
-		if(word_type == WORD_REDIRECT_OUT)
-			printf("word_type REDIR out and i value is : %d\n", i);
-		if(word_type == WORD_REDIRECT_APPEND)
-			printf("word_type REDIR append and i value is : %d\n", i);
-		if(word_type == WORD_SPACE)
-			printf("word_type SPACE AFTER WORD and i value is : %d\n", i);
-		if(word_type == WORD_SPACE_AFTER_REDIRECT)
-			printf("word_type SPACE AFTER REDIRECT and i value is : %d\n", i);
-		if(word_type == WORD_END_OF_SINGLE_QUOTE)
-			printf("word_type END OF SINGLE QUOTE and i value is : %d\n", i);
-		if(word_type == WORD_END_OF_DOUBLE_QUOTE)
-			printf("word_type END OF DOUBLE QUOTE and i value is : %d\n", i);
-		if(word_type == WORD_END_STANDARD)
-			printf("word_type END OF STANDARD and i value is : %d\n", i);
-		if(word_type == WORD_END_STANDARD_TO_SINGLE_QUOTE)
-			printf("word_type END OF STANDARD TO SINGLE QUOTE and i value is : %d\n", i);
-		if(word_type == WORD_END_STANDARD_TO_DOUBLE_QUOTE)
-			printf("word_type END OF STANDARD TO DOUBLE QUOTE and i value is : %d\n", i);
-		//----------------------------------------------------------------------------
-		if ((word_type == WORD_ERROR))
+		if ((word_type == W_ERROR))
 		{
 			printf("Word type error with i loop  value of %d\n", i);
 			break;
 		}
-		if (word_type == WORD_END)
-		break;
-		if (word_type == WORD_SINGLE_QUOTE || word_type == WORD_DOUBLE_QUOTE || word_type == WORD_STANDARD)
+		if (word_type == W___END)
+			break;
+		if (word_type == W_SINGQ || word_type == W_DOUBQ || word_type == W_STNDR)
 		{
 			random_command = extract_command(valid_command,&i, matrix, word_type, previous_word_type);
+			if (random_command)
+			{
+				command = create_command_part(random_command, word_type);
+				add_command_part_to_list(&lst, command);
+			}
 			printf("random command is %s\n", random_command);
-			printf("Salto del largo de la cadena %d\n", ft_strlen(random_command));
+			printf("Salto del largo de la cadena %d\n", i);
 			continue;
 		}
 		i++;
 	}
-	printf("random command is %s\n", random_command);
+	t_command_part *tmp = lst;
+	while (tmp)
+	{
+		printf("Valor: %s, Tipo: %d\n", tmp->value, tmp->type);
+		tmp = tmp->next;
+	}
 	return (tokens);
 }
 
+char	**tokenize(char *valid_command)
+{
+	const int	matrix[W_TOTAL][NUM_INPUT] = {
+	{W_START, W_STNDR, W___END, W_SINGQ, W_DOUBQ, W_REDIN, W_REDOU}, //W_START
+	{W_EOFST, W_STNDR, W___END, W_EOSTS, W_EOSTD, W_EOFST, W_EOFST}, // W_STNDR
+	{W_SINGQ, W_SINGQ, W_ERROR, W_EOFSQ, W_SINGQ, W_SINGQ, W_SINGQ}, // WORD_SINGLE QUOTE
+	{W_DOUBQ, W_DOUBQ, W_STNDR, W_DOUBQ, W_EOFDQ, W_DOUBQ, W_DOUBQ}, // WORD_DOUBLE QUOTE
+	{W_SPACE, W_STNDR, W_ERROR, W_SINGQ, W_DOUBQ, W_HRDOC, W_ERROR}, // REDIRECT_IN
+	{W_REDOU, W_REDOU, W___END, W_REDOU, W_REDOU, W_REDIN, W_REDAP}, // REDIRECT_OUT
+	{W_SARED, W_STNDR, W_ERROR, W_SINGQ, W_DOUBQ, W_ERROR, W_ERROR}, // REDIRECT_APPEND
+	{W_SARED, W_STNDR, W_ERROR, W_SINGQ, W_DOUBQ, W_ERROR, W_ERROR}, // HERE_DOC
+	{W_SPACE, W_STNDR, W___END, W_SINGQ, W_DOUBQ, W_REDIN, W_REDOU}, // SPACE AFTER WORD
+	{W_SARED, W_STNDR, W_ERROR, W_SINGQ, W_DOUBQ, W_ERROR, W_ERROR}, // SPACE AFTER REDIRECT
+	{W_SPACE, W_STNDR, W___END, W_SINGQ, W_DOUBQ, W_REDIN, W_REDOU}, // END OF SINGLE QUOTE
+	{W_SPACE, W_STNDR, W___END, W_SINGQ, W_DOUBQ, W_REDIN, W_REDOU}, // END OF DOUBLE QUOTE
+	{W_SPACE, W_STNDR, W___END, W_SINGQ, W_DOUBQ, W_REDIN, W_REDOU}, // END OF STANDARD
+	{W_DOUBQ, W_DOUBQ, W___END, W_EOFDQ, W_EOFDQ, W_DOUBQ, W_DOUBQ}, // END OF STANDARD TO DOUBLE QUOTE
+	{W_SINGQ, W_SINGQ, W___END, W_EOFSQ, W_SINGQ, W_SINGQ, W_SINGQ} // END OF STANDARD TO DOUBLE QUOTE
+	};
+
+	token = tokenize_help(matrix, valid_command);
+}
 
 //probar los siguientes comandos en casa
 // "abcde" abcd abcdef
 // "p"w'd'
+		// //--------------------------------------------------------------------
+		// printf("---------------------------------------\n");
+		// printf("Letra %c\n", valid_command[i]);
+		// if(word_type == W_START)
+		// 	printf("word_type START and i value is : %d\n", i);
+		// if(word_type == W_STNDR)
+		// 	printf("word_type WORD and i value is : %d\n", i);
+		// if(word_type == W_SINGQ)
+		// 	printf("word_type SINGLE QUOTE and i value is : %d\n", i);
+		// if(word_type == W_DOUBQ)
+		// 	printf("word_type DOUBLE and i value is : %d\n", i);
+		// if(word_type == W_REDIN)
+		// 	printf("word_type REDIR in and i value is : %d\n", i);
+		// if(word_type == W_REDOU)
+		// 	printf("word_type REDIR out and i value is : %d\n", i);
+		// if(word_type == W_REDAP)
+		// 	printf("word_type REDIR append and i value is : %d\n", i);
+		// if(word_type == W_SPACE)
+		// 	printf("word_type SPACE AFTER WORD and i value is : %d\n", i);
+		// if(word_type == W_SARED)
+		// 	printf("word_type SPACE AFTER REDIRECT and i value is : %d\n", i);
+		// if(word_type == W_EOFSQ)
+		// 	printf("word_type END OF SINGLE QUOTE and i value is : %d\n", i);
+		// if(word_type == W_EOFDQ)
+		// 	printf("word_type END OF DOUBLE QUOTE and i value is : %d\n", i);
+		// if(word_type == W_EOFST)
+		// 	printf("word_type END OF STANDARD and i value is : %d\n", i);
+		// if(word_type == W_EOSTS)
+		// 	printf("word_type END OF STANDARD TO SINGLE QUOTE and i value is : %d\n", i);
+		// if(word_type == W_EOFST_TO_DOUBLE_QUOTE)
+		// 	printf("word_type END OF STANDARD TO DOUBLE QUOTE and i value is : %d\n", i);
+		// //----------------------------------------------------------------------------
+
+
+
+	// printf("valor de i es %d\n", *i);
+	// printf("valor de start es %d\n", start);
+	// printf("Numero de caracteres impresos son %d\n", *i - start);
