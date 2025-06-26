4580a501286fe80f3a5f12140bfaf362b211e289
Igbescobar <igbescobar@gmail.com>
2025-06-26 12:28:24 +0200

    fix to echo with multiple -n

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,24 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: igngonza <igngonza@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: igngonza <igngonza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 12:29:56 by igngonza          #+#    #+#             */
-/*   Updated: 2025/06/23 17:59:48 by igngonza         ###   ########.fr       */
+/*   Updated: 2025/06/26 12:27:39 by igngonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+int	is_valid_n_flag(const char *s)
+{
+	int	i;
+
+	if (s[0] != '-')
+		return (0);
+	i = 1;
+	while (s[i])
+	{
+		if (s[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 int	builtin_echo(char **args, t_shell *shell)
 {
 	int	newline;
 	int	i;
 
-	i = 1;
 	newline = 1;
+	i = 1;
 	(void)shell;
-	if (args[1] && ft_strncmp(args[1], "-n", 2) == 0)
+	while (args[i] && is_valid_n_flag(args[i]))
 	{
 		newline = 0;
 		i++;
